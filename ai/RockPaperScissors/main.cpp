06edfa2b9c5108d85da73044d517d574fa701d6e
Plum-Tree-Garden <73845528+Plum-Tree-Garden@users.noreply.github.com>
2021-08-02 19:30:58 -0400

    Create main.cpp

---
+++ ai/RockPaperScissors/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+#include "randomSimulation.h"
+
+
+using namespace std;
+using namespace rps;
+
+// main begin
+int main() 
+{
+
+	randomSimulation a(10);
+	a.display();
+
+
+
+	return 0;
+
+// main end
+}
