43051c46c7fc8afc92e16a15d36848ded26b8b10
Plum-Tree-Garden <73845528+Plum-Tree-Garden@users.noreply.github.com>
2021-08-18 00:33:03 -0400

    Create template1.cpp

---
+++ CODING_COMPETITION/templates/template1.cpp
@@ -0,0 +1,36 @@
+// plum tree garden
+// blank template for coding competition
+// g++ -std=c++14 -02 -Wall hero.cpp -o hero
+
+// include entire standard library
+#include <bits/stdc++.h>
+  // #define F first
+  // #define S second
+  // #define PB push_back
+  // #define MP make_pair
+
+
+using namespace std;
+
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  
+  // printf()
+  // scanf()
+  // string s;
+  // getline(cin, s)
+  // while(cin >> x)
+  
+  // typedef long long ll;
+  // typedef vector<int> vi;
+  // typedef vector<int, int> pi;
+  
+
+
+  return 0;
+}
